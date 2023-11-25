1dcaeaed5f3fe0706d3a77cf06fe5ccbe6bf5a1f
Gahan Rakholia <gahan94rakh@gmail.com>
2023-11-25 22:26:33 +0530

    add subcommand for version and run

--- src/calculator.cr
+++ src/calculator.cr
@@ -6,11 +6,20 @@ operand1 : Int32 = 0
 operand2 : Int32 = 0
 operator : String = "-"
 
+version = false
+run = false
+
 parser = OptionParser.parse do |parser|
-  parser.banner = "Usage: calculator [args]"
-  parser.on("-o1 OPERAND1", "--operand1=OPERAND1", "first operator") { |val| operand1 = val.to_i }
-  parser.on("-o2 OPERAND2", "--operand2=OPERAND2", "second operator") { |val| operand2 = val.to_i }
-  parser.on("-op OPERAND", "--operator2=OPERAND", "second operator") { |val| operand = val }
+  parser.banner = "Usage: calculator [subcommands] [args]"
+  parser.on("version", "Current version of CLI") do
+    version = true
+  end
+  parser.on("run", "run the app") do
+    run = true
+    parser.on("-a OPERAND1", "--operand1=OPERAND1", "first operator") { |val| operand1 = val.to_i }
+    parser.on("-b OPERAND2", "--operand2=OPERAND2", "second operator") { |val| operand2 = val.to_i }
+    parser.on("-c OPERAND", "--operator2=OPERAND", "second operator") { |val| operand = val }
+  end
   parser.on("-h", "--help", "Show this help") do
     puts parser
     exit
@@ -25,4 +34,11 @@ end
 
 parser.parse
 
-puts Calculator::App.run(operand1: operand1, operand2: operand2, operator: operator)
+if version
+  puts Calculator::VERSION
+elsif run
+  puts Calculator::App.run(operand1, operand2, operator)
+else
+  puts parser
+  exit
+end
